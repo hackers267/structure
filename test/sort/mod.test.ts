b0fef3814c020d12e426101b35932a39ab33c610
silence_zhpf <silence_zhpf@aliyun.com>
2020-10-02 18:38:28 +0800

    :refactor 重构测试代码
    重构测试代码，使代码更有语义，更简洁

--- test/sort/mod.test.ts
+++ test/sort/mod.test.ts
@@ -2,22 +2,25 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { bubbleSort } from "../../src/sort/bubble.ts";
 import { insertSort } from "../../src/sort/insert.ts";
 
+function init() {
+  const array: number[] = [2, 12, 19, 8, 4];
+  const result = array.slice().sort((a, b) => a > b ? 1 : -1);
+  return { array, actual: result };
+}
+
 Deno.test({
   name: "Bubble Sort",
   fn: () => {
-    const array: number[] = [2, 12, 19, 8, 4];
-    const actual = bubbleSort(array);
-    const result = array.slice().sort((a, b) => a > b ? 1 : -1);
-    assertEquals(actual, result);
+    const { array, actual } = init();
+    const result = bubbleSort(array);
+    assertEquals(result, actual);
   },
 });
 Deno.test({
   name: "Insert Sort",
   fn: () => {
-    const array: number[] = [2, 12, 19, 8, 4];
-    const actual = insertSort(array);
-    console.log(actual);
-    const result = array.slice().sort((a, b) => a > b ? 1 : -1);
-    assertEquals(actual, result);
+    const { array, actual } = init();
+    const result = insertSort(array);
+    assertEquals(result, actual);
   },
 });
